1518ec0e199b62929cddcef04e795cf67cc4b293
thuan411loss <thuanlcse180399@fpt.edu.vn>
2025-07-06 14:34:27 +0700

    patient

--- src/main/java/vn/BE_SWP302/domain/response/PatientDTO.java
+++ src/main/java/vn/BE_SWP302/domain/response/PatientDTO.java
@@ -1,10 +1,14 @@
 package vn.BE_SWP302.domain.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatientDTO {
     private Long bookingId;
     private String name;
